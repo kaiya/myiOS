f22870478b141e1a1a8d5e9f6e8fbb91c27fec6c
K.Yawn <K.Yawn@icloud.com>
2015-03-13 20:14:01 -0700

    GithubTestDemo

---
+++ GithubTestDemo/GithubTestDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GithubTestDemo
+//
+//  Created by K.Yawn Xoan on 3/13/15.
+//  Copyright (c) 2015 K.Yawn Xoan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var cities = ["fuyang","hefei","wuhan"]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
