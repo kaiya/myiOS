05135bc14f516c89d98344a4cff9f900dd2573d8
K.Yawn <K.Yawn@icloud.com>
2015-03-13 20:31:46 -0700

    myCityTest

--- GithubTestDemo/GithubTestDemo/ViewController.swift
+++ GithubTestDemo/GithubTestDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var cities = ["fuyang","hefei","wuhan"]
+        var myCity = "Kyawn"
     }
 
     override func didReceiveMemoryWarning() {
